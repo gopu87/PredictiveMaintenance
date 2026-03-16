c79af1d4c8c24cc3359330440dbb730c5dc1734c
gopu87 <gopalakrishnanj999@gmail.com>
2026-03-16 09:20:19 +0000

    Final Push

---
+++ app.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+import pandas as pd
+import joblib
+
+st.title("🚢 Rolls-Royce Predictive Maintenance")
+st.write("Symmetry 9 Deployment Active.")
+# Add your model loading and prediction logic here
